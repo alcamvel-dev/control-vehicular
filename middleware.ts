90c6ce4fed9a7289b26e325179ad374cfaa49eb5
Alejandro Camacho <alcamvel@gmail.com>
2026-05-15 02:11:48 -0600

    Disable middleware

--- middleware.ts
+++ middleware.ts
@@ -1,51 +1,8 @@
-import {
+import {import {
   NextResponse
 } from 'next/server'
 
-import type {
-  NextRequest
-} from 'next/server'
-
-export function middleware(
-  request: NextRequest
-) {
-
-  const token =
-    request.cookies.get(
-      'sb-access-token'
-    )
-
-  const rutasProtegidas = [
-
-    '/portal',
-    '/dashboard',
-    '/clientes',
-    '/vehiculos',
-    '/documentos',
-    '/vencimientos',
-    '/expediente'
-
-  ]
-
-  const proteger =
-    rutasProtegidas.some(
-      (ruta) =>
-        request.nextUrl.pathname.startsWith(ruta)
-    )
-
-  if (
-    proteger &&
-    !token
-  ) {
-
-    return NextResponse.redirect(
-      new URL(
-        '/login',
-        request.url
-      )
-    )
-
-  }
+export function middleware() {
 
   return NextResponse.next()
 
