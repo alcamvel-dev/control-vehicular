fb8680f7c6c26e559ab1d21a6c16124c3d66b53d
Alejandro Camacho <alcamvel@gmail.com>
2026-05-20 16:05:37 -0600

    Disable middleware temp

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,7 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-
-  const token = request.cookies.get('sb-access-token')
-
-  // SI NO EXISTE LOGIN
-  if (!token) {
-
-    return NextResponse.redirect(
-      new URL('/login', request.url)
-    )
-
-  }
+export function middleware() {
 
   return NextResponse.next()
-
-}
-
-export const config = {
-
-  matcher: [
-
-    '/admin/:path*',
-    '/portal/:path*',
-
-  ],
 
 }
