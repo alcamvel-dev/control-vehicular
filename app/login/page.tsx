01395a420776a4acb8f1156d802394e8086e7a86
Alejandro Camacho <alcamvel@gmail.com>
2026-05-20 13:54:16 -0600

    Fix login final

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,67 +13,67 @@ export default function LoginPage() {
 
   const handleLogin = async () => {
 
-    const { data, error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
 
-      email,
-      password,
+    email,
+    password,
 
-    })
+  })
 
-    if (error) {
+  if (error) {
 
-      alert(error.message)
-      return
-
-    }
-
-    const userId = data.user.id
-
-    console.log('USER ID:', userId)
-
-    const { data: usuarios, error: userError } = await supabase
-      .from('usuarios')
-      .select('*')
-      .eq('auth', `${userId}`)
-
-    console.log('USUARIOS:', usuarios)
-
-    if (userError) {
-
-      console.log(userError)
-      alert('Error buscando usuario')
-      return
-
-    }
-
-    if (!usuarios || usuarios.length === 0) {
-
-      alert('Usuario sin permisos')
-      return
-
-    }
-
-    const usuario = usuarios[0]
-
-    console.log('ROL:', usuario.rol)
-
-    if (usuario.rol === 'admin') {
-
-      router.push('/admin')
-      return
-
-    }
-
-    if (usuario.rol === 'cliente') {
-
-      router.push('/portal')
-      return
-
-    }
-
-    alert('Rol no válido')
+    alert(error.message)
+    return
 
   }
+
+  const userEmail = data.user.email
+
+  console.log('EMAIL:', userEmail)
+
+  const { data: usuarios, error: userError } = await supabase
+    .from('usuarios')
+    .select('*')
+    .eq('correo', userEmail)
+
+  console.log('USUARIOS:', usuarios)
+
+  if (userError) {
+
+    console.log(userError)
+    alert('Error buscando usuario')
+    return
+
+  }
+
+  if (!usuarios || usuarios.length === 0) {
+
+    alert('Usuario sin permisos')
+    return
+
+  }
+
+  const usuario = usuarios[0]
+
+  console.log('ROL:', usuario.rol)
+
+  if (usuario.rol === 'admin') {
+
+    router.push('/admin')
+    return
+
+  }
+
+  if (usuario.rol === 'cliente') {
+
+    router.push('/portal')
+    return
+
+  }
+
+  alert('Rol no válido')
+
+}
 
   return (
 
